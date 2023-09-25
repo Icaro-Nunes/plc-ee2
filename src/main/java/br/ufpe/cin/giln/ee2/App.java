d5b72a40bbc2dd25de431486af27ee70f54c1f93
Icaro-Nunes <icarolimanunes@gmail.com>
2023-09-25 15:38:30 -0300

    feat(setup): add test case file parsing

--- src/main/java/br/ufpe/cin/giln/ee2/App.java
+++ src/main/java/br/ufpe/cin/giln/ee2/App.java
@@ -1,5 +1,12 @@
 package br.ufpe.cin.giln.ee2;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+
 import br.ufpe.cin.giln.ee2.common.RunnableProblem;
+import br.ufpe.cin.giln.ee2.p1.P1;
 
 /**
  * Hello world!
@@ -9,8 +16,30 @@ public class App
 {
     public static void main( String[] args )
     {
+        try {
+            BufferedReader file = new BufferedReader(new FileReader("testData/p1/test1.txt"));
 
-        RunnableProblem prog;
-        prog.run(args);
+            ArrayList<String> params = new ArrayList<>();
+
+            String line = file.readLine();
+
+            while(line != null){
+                params.add(line);
+                line = file.readLine();
+            }
+
+            RunnableProblem prog = new P1();
+
+            if(
+                !prog.run(
+                    params.toArray(new String[params.size()])
+                )
+            )
+                System.out.println("Problema na execução!!");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("File not Found!");
+        }
     }
 }
