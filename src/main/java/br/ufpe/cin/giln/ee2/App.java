55c5c24284ab116b577e076ad4b800b1ef02ed3d
Icaro-Nunes <icarolimanunes@gmail.com>
2023-09-25 10:03:27 -0300

    Initial commit

---
+++ src/main/java/br/ufpe/cin/giln/ee2/App.java
@@ -0,0 +1,13 @@
+package br.ufpe.cin.giln.ee2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
