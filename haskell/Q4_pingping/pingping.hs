4bf13f151f0f5aaab206e083de2298b9662370ab
Icaro-Nunes <icarolimanunes@gmail.com>
2023-09-27 16:34:38 -0300

    chore: add comments

--- haskell/Q4_pingping/pingping.hs
+++ haskell/Q4_pingping/pingping.hs
@@ -1,10 +1,18 @@
 module Pingping where
-import Control.Concurrent (MVar, takeMVar, putMVar)
-import Control.Monad (replicateM)
+import Control.Concurrent
+import Control.Monad (replicateM, replicateM_)
+import Data.Maybe
 
 popFirst :: [a] -> (Maybe a, [a])
 popFirst [] = (Nothing, [])
-popFirst (a: ls) = (Just a, ls)
+popFirst l = (Just $ head l, tail l)
+
+counterInc :: MVar Int -> IO Int
+counterInc v = do
+        val <- takeMVar v
+        let inc = val+1
+        putMVar v inc
+        return val
 
 data PingPing = PingPing {
     queue :: MVar [Int],
@@ -12,19 +20,63 @@ data PingPing = PingPing {
 }
 
 consume :: Int -> IO ()
-consume i = putStrLn $ show i
+consume i = do
+        putStrLn $ "received "++ show i
+        return ()
 
-pingSenderAux :: PingPing -> Int -> IO ()
-pingSenderAux game n = do
+pingSenderAux :: PingPing -> IO Int -> IO ()
+pingSenderAux game numGen = do
+        n <- numGen
         q <- takeMVar (queue game)
+        putStrLn $ "put "++ show n ++ " on queue"
         putMVar (queue game) (q++[n])
 
-pingSender :: PingPing -> Int -> IO()
-pingSender game n = replicateM n (pingSenderAux game n)
+pingSender :: PingPing -> Int -> IO ()
+pingSender game n = do
+        counter <- newMVar 0
+        replicateM_ n (pingSenderAux game (counterInc counter))
+        takeMVar (completed game)
+        putMVar (completed game) True
 
+
+pingReceiverLoop :: PingPing -> IO ()
+pingReceiverLoop game = do
+        q <- takeMVar (queue game)
+        done <- takeMVar (completed game)
+        putMVar (completed game) done
+        if done && (length q == 0) then do
+            putMVar (queue game) q
+            return ()
+        else do
+            let (it, sq) = popFirst q
+            if not (isNothing it) then do
+                let Just i = it
+                consume i
+                putMVar (queue game) sq
+                pingReceiverLoop game
+            else do
+                putMVar (queue game) sq
+                pingReceiverLoop game
+
+
+pingReceiver :: PingPing -> IO ()
+pingReceiver game = pingReceiverLoop game
 
 
 main :: IO ()
-main = do
-    undefined
+main = do 
+    completed <- newMVar False
+    queue <- newMVar []
+    let game = PingPing queue completed
+    let n = 30
+    senderThread <- forkIO $ (pingSender game n)
+    threadDelay (300)
+    receiverThread <- forkIO $ (pingReceiver game)
+    threadDelay (n * 50 * 1000)
+    killThread senderThread
+    killThread receiverThread
+    
+
+
+
 
